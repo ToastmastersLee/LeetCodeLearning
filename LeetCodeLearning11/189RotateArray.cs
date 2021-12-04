3da44ff651cbdab1c675315609c6def8c87fde58
Varian.Wrynn <yuna1111@163.com>
2021-12-04 22:57:38 +0800

    2021年12月4日22:57:38

---
+++ LeetCodeLearning11/189RotateArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeLearning11
+{
+    public class RotateArray
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            /*假如它移动的是3次，6次，9次，12次.....390次？
+             * 位置应该都是和移动3次是一样结构，所以取模*/
+            k = k % nums.Length;
+            Reverse(nums, 0, nums.Length -1);
+            Reverse(nums, 0, k - 1);
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        public void Reverse(int[] nums,int start, int end)
+        {
+            while (start < end)
+            {
+                int temp = nums[start];
+                nums[start] = nums[end];
+                nums[end] = nums[temp];
+                start++;
+                end--;
+            }
+
+        }
+    }
+}
--- LeetCodeLearning11/977SquaresSortedArray.cs
+++ LeetCodeLearning11/977SquaresSortedArray.cs
@@ -4,16 +4,16 @@ using System.Text;
 
 namespace LeetCodeLearning11
 {
-  public  class SquaresSortedArray
+    public class SquaresSortedArray
     {
         public int[] SortedSquares(int[] nums)
         {
             int left = 0, right = nums.Length - 1;
-            int[] result = new int[] { nums.Length};
+            int[] result = new int[] { nums.Length };
             int index = result.Length;
-            while(left<=right)
+            while (left <= right)
             {
-                if(nums[left] * nums[left] > nums[right] * right[*])
+                if (nums[left] * nums[left] > nums[right] * right[*])
                 {
                     result[index--] = nums[left] * nums[left];
                     left++;
