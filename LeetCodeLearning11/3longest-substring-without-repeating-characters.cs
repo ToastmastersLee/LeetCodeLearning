a76c31b9fe2f410a45699ee0bd83e1800ec71c0c
Varian.Wrynn <yuna1111@163.com>
2021-12-21 00:08:12 +0800

    2021年12月21日00:08:26

---
+++ LeetCodeLearning11/3longest-substring-without-repeating-characters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeLearning11
+{
+    public  class _3longest_substring_without_repeating_characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            // s = "pwwkew"
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            int maxLen = 0; //用于记录最大不重复子串的长度
+            int left = 0; //滑动窗口做指针
+            for(int i=0;i<s.Length;i++)
+            {
+                if(map.ContainsKey(s[i]))
+                {
+                    left = Math.Max(left, map[s[i]] + 1);
+                }
+                map[s[i]] = i;
+                maxLen = Math.Max(maxLen, i - left + 1);
+            }
+            return maxLen;
+        }
+    }
+}
--- LeetCodeLearning11/Program.cs
+++ LeetCodeLearning11/Program.cs
@@ -11,8 +11,12 @@ namespace LeetCodeLearning11
             //SearchInsertClass sic = new SearchInsertClass();
             //var retVal = sic.SearchInsert(new[] { 1, 3, 5, 6 }, 2);
 
-            _557ReverseWordsInString3 resversClass = new _557ReverseWordsInString3();
-            var retVal = resversClass.ReverseWords2("Let's take LeetCode contest");
+            //_557ReverseWordsInString3 resversClass = new _557ReverseWordsInString3();
+            //var retVal = resversClass.ReverseWords2("Let's take LeetCode contest");
+            //Console.WriteLine(retVal);
+
+            _3longest_substring_without_repeating_characters longS = new _3longest_substring_without_repeating_characters();
+            var retVal = longS.LengthOfLongestSubstring("pwwkew");
             Console.WriteLine(retVal);
             Console.WriteLine("Hello World!");
         }
