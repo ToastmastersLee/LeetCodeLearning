746560456b5e17b373f8c752c7bd6617cd2018f2
ToastmastersLee <leewongnetwork@gmail.com>
2023-09-08 18:25:56 +0800

    ss

--- python/data_processing.py
+++ python/data_processing.py
@@ -5,8 +5,9 @@ with open(ini_path, 'r') as f:
     for line in f:
         key, value = line.strip().split('=')
         ini_data[key] = value
-xml_data = {'Name': 'Lee', 'Name': '妍子'}  # 你可以使用适合的XML库来处理XML文件
+#xml_data = {'Name': 'Lee2', 'Name2': '妍子2'}  # 你可以使用适合的XML库来处理XML文件
 
-df = pd.DataFrame([ini_data, xml_data])
+#df = pd.DataFrame([ini_data, xml_data])
+df =pd.DataFrame(ini_data)
 excel_path = 'output1.xlsx'
 df.to_excel(excel_path, index=False)
